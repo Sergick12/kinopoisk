cc792214db040fbeae6cd831fc4bbd6078e3100a
Sergick12 <cepgruddd1@gmail.com>
2023-02-11 09:39:30 +0300

    run rubocop

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -12,6 +12,6 @@
 #
 require 'rails_helper'
 
-RSpec.describe Movie, type: :model do
+RSpec.describe Movie do
   pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/views/movies/edit.html.erb_spec.rb
+++ spec/views/movies/edit.html.erb_spec.rb
@@ -1,28 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe "movies/edit", type: :view do
-  let(:movie) {
+RSpec.describe 'movies/edit' do
+  let(:movie) do
     Movie.create!(
-      name: "MyString",
-      description: "MyString",
+      name: 'MyString',
+      description: 'MyString',
       age_linit: 1
     )
-  }
+  end
 
-  before(:each) do
+  before do
     assign(:movie, movie)
   end
 
-  it "renders the edit movie form" do
+  it 'renders the edit movie form' do
     render
 
-    assert_select "form[action=?][method=?]", movie_path(movie), "post" do
+    assert_select 'form[action=?][method=?]', movie_path(movie), 'post' do
+      assert_select 'input[name=?]', 'movie[name]'
 
-      assert_select "input[name=?]", "movie[name]"
+      assert_select 'input[name=?]', 'movie[description]'
 
-      assert_select "input[name=?]", "movie[description]"
-
-      assert_select "input[name=?]", "movie[age_linit]"
+      assert_select 'input[name=?]', 'movie[age_linit]'
     end
   end
 end
