5c0e5654c17fae7ff079b7a32cc38b0af5f3132d
Sergick12 <cepgruddd1@gmail.com>
2023-02-11 10:22:33 +0300

    add validates

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_11_061627) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_11_064833) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "movies", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.date "release_date"
-    t.integer "age_linit"
+    t.string "name", null: false
+    t.string "description", null: false
+    t.date "release_date", null: false
+    t.integer "age_linit", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
