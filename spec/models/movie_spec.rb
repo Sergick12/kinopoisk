048287e599f369fc9c04335d0eb9b1650f63ba46
Sergick12 <cepgruddd1@gmail.com>
2023-02-11 09:32:00 +0300

    run annotate from model movie

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id           :bigint           not null, primary key
+#  name         :string
+#  description  :string
+#  release_date :date
+#  age_linit    :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
