514bea5d46c9c0f9583b4de6894f78ec2df6a1b6
Sergick12 <cepgruddd1@gmail.com>
2023-02-11 10:22:33 +0300

    add validates

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,12 +3,16 @@
 # Table name: movies
 #
 #  id           :bigint           not null, primary key
-#  name         :string
-#  description  :string
-#  release_date :date
-#  age_linit    :integer
+#  name         :string           not null
+#  description  :string           not null
+#  release_date :date             not null
+#  age_linit    :integer          not null
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
 #
 class Movie < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :age_linit, presence: true
+  validates :release_date, presence: true
+  validates :description, presence: true
 end
