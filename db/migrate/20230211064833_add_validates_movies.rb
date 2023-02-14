4ccebf6350b6b7152f25b419c1fef34f65c7d442
Sergick12 <cepgruddd1@gmail.com>
2023-02-14 17:57:35 +0300

    removing comments

--- db/migrate/20230211064833_add_validates_movies.rb
+++ db/migrate/20230211064833_add_validates_movies.rb
@@ -1,13 +1,8 @@
 class AddValidatesMovies < ActiveRecord::Migration[7.0]
   def change
-    # validates :name, presence: true, length: { minimum: 2 }
-    # validates :age_linit, presence: true
-    # validates :release_date, presence: true
-    # validates :description, presence: true
     change_column_null :movies, :name, false
     change_column_null :movies, :age_linit, false
     change_column_null :movies, :release_date, false
     change_column_null :movies, :description, false
-
   end
 end
