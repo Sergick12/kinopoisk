9bb346b9c40d1d73127ae610a68c7577f5df6d5f
Sergick12 <cepgruddd1@gmail.com>
2023-02-15 23:15:01 +0300

    add spec model

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -13,5 +13,13 @@
 require 'rails_helper'
 
 RSpec.describe Movie do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    subject { build(:movie) }
+
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:release_date) }
+    it { is_expected.to validate_presence_of(:age_linit) }
+    it { is_expected.to validate_length_of(:name).is_at_least(2) }
+  end
 end
