3fc6c23167ca00ce14dc9ab08d70607f62797332
Sergick12 <cepgruddd1@gmail.com>
2023-02-18 14:01:56 +0300

    add spec validations model

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Genre do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_length_of(:name).is_at_least(2) }
 end
