f7d77041be594086412ce076becfe3f4a666d0c8
Sergick12 <cepgruddd1@gmail.com>
2023-04-01 22:05:46 +0300

    add associations and validations in models

--- app/models/gemres_movie.rb
+++ app/models/gemres_movie.rb
@@ -1,4 +1,6 @@
 class GemresMovie < ApplicationRecord
   belongs_to :movie
   belongs_to :genres
+
+  validates :movie_id, uniqueness: { scope: :genre_id }
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,8 @@
 class Genre < ApplicationRecord
+  has_many :genres_movies
+  has_many :movies, through: :genres_movies
+
+  validates_associated :movies
+
   validates :name, presence: true, length: { minimum: 2 }
 end
