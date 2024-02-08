478bf32b6bea869f171001bc4f90b92c9880be87
Sergick12 <cepgruddd1@gmail.com>
2024-02-08 14:05:53 +0300

    Create mein page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/help'
   resources :genres, shallow: true do
     resources :movies
   end
